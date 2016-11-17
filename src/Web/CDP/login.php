b94a9460ca420f84ecaf9d00f7808d680e5bd545
Nzaoui <naji.zaoui@hotmail.fr>
2016-11-17 20:30:59 +0100

    rectif Login.php

--- src/Web/CDP/login.php
+++ src/Web/CDP/login.php
@@ -1,8 +1,4 @@
-<?php
 
-session_start();
-
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -63,7 +59,7 @@ session_start();
        <section class="panel default blue_title h2">
        <div class="panel-body">
 		  <div class="login_content">
-		 <form role="form" class="form-horizontal" action="login_check.php" method="Post">
+		 <form role="form" class="form-horizontal" action="login.php" method="Post">
 			  <div class="form-group">
 				
 				<div class="col-sm-10">
@@ -83,9 +79,11 @@ session_start();
 					  <input type="checkbox"><p class="pull-left"> Remember me</p></label>
 	
 					  <button class="btn btn-default pull-right" type="submit" name="submit">Se connecter</button>
-					  </div>
-					   
+					  </div>					  
 				</div>
+				<?php 
+					include("login_check.php"); 
+					?> 
 				<br>
 				
 			  </div>
